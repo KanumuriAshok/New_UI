44c611d0161bbf690b692a731bcfc5dc2facb772
YersultanSaduakhas <zver-yersul@mail.ru>
2022-10-26 16:58:10 +0600

    pn boundary check

--- ui/src/app/connection-handeling/connection-handeling.component.ts
+++ ui/src/app/connection-handeling/connection-handeling.component.ts
@@ -8,23 +8,23 @@ import { environment } from '../../environments/environment';
 })
 export class ConnectionHandelingComponent implements OnInit {
   constructor(private httpClient: HttpClient) {}
-  apiUrl = environment.api+'/api/shape_file_page';
+  apiUrl = environment.api + '/api/shape_file_page';
   loadingLoader = false;
- 
+
   uploadFilesStatus = 'none_status'; //success,fail
   uploadRefFilesStatus = 'none_status'; //success,fail
   extractExistingStatus = 'none_status'; //success,fail
   missingDemandStatus = 'none_status'; //success,fail
   secondaryPreprocessgpStatus = 'none_status'; //success,fail
-  pnBoundaryCheckStatus = 'none_status'; //success,fail
+  pnBoundaryCheckStatus = ''; //success,fail
   exportStatus = 'none_status'; //success,fail
   googleFetchStatus = 'none_status'; //success,fail
 
-  userType:any;
+  userType: any;
 
   sigdisplay = 'none';
   ngOnInit(): void {
-    this.userType= localStorage.getItem('usertype');
+    this.userType = localStorage.getItem('usertype');
     console.log(this.userType);
   }
 
@@ -55,7 +55,7 @@ export class ConnectionHandelingComponent implements OnInit {
       }
     }
 
-    this.httpClient.post(environment.api+this.apiUrl, formData).subscribe(
+    this.httpClient.post(environment.api + this.apiUrl, formData).subscribe(
       (res) => {
         alert('upload completed successfully');
         this.uploadFilesStatus = 'green_status';
@@ -72,89 +72,98 @@ export class ConnectionHandelingComponent implements OnInit {
     this.loadingLoader = true;
     const formData = new FormData();
     formData.append('username', localStorage.getItem('username'));
-    this.httpClient.post(environment.api+'/api/secondary_structures', formData).subscribe(
-      (res) => {
-        alert('completed successfully');
-        this.loadingLoader = false;
-        this.extractExistingStatus = 'green_status';
-        this.missingDemand();
-      },
-      (err) => {
-        console.error(err);
-        alert('completed with error');
-        this.extractExistingStatus = 'red_status';
-        this.loadingLoader = false;
-      },
-      () => {
-        this.loadingLoader = false;
-      }
-    );
+    this.httpClient
+      .post(environment.api + '/api/secondary_structures', formData)
+      .subscribe(
+        (res) => {
+          alert('completed successfully');
+          this.loadingLoader = false;
+          this.extractExistingStatus = 'green_status';
+          this.missingDemand();
+        },
+        (err) => {
+          console.error(err);
+          alert('completed with error');
+          this.extractExistingStatus = 'red_status';
+          this.loadingLoader = false;
+        },
+        () => {
+          this.loadingLoader = false;
+        }
+      );
   }
   missingDemand() {
     this.loadingLoader = true;
     const formData = new FormData();
     formData.append('username', localStorage.getItem('username'));
-    this.httpClient.post(environment.api+'/api/secondary_preprocessdp', formData).subscribe(
-      (res) => {
-        alert('completed successfully');
-        this.loadingLoader = false;
-        this.missingDemandStatus = 'green_status';
-        this.export();
-      },
-      (err) => {
-        console.error(err);
-        alert(' completed with error');
-        this.missingDemandStatus = 'red_status';
-      },
-      () => {
-        this.loadingLoader = false;
-      }
-    );
+    this.httpClient
+      .post(environment.api + '/api/secondary_preprocessdp', formData)
+      .subscribe(
+        (res) => {
+          alert('completed successfully');
+          this.loadingLoader = false;
+          this.missingDemandStatus = 'green_status';
+          this.export();
+        },
+        (err) => {
+          console.error(err);
+          alert(' completed with error');
+          this.missingDemandStatus = 'red_status';
+        },
+        () => {
+          this.loadingLoader = false;
+        }
+      );
   }
   secondaryPreprocessgp() {
     //
     this.loadingLoader = true;
     const formData = new FormData();
     formData.append('username', localStorage.getItem('username'));
-    this.httpClient.post(environment.api+'/api/secondary_preprocessgp', formData).subscribe(
-      (res) => {
-        alert('completed successfully');
-        this.loadingLoader = false;
-        this.secondaryPreprocessgpStatus = 'green_status';
-        this.pnBoundaryCheck();
-      },
-      (err) => {
-        console.error(err);
-        alert(' completed with error');
-        this.loadingLoader = false;
-        this.secondaryPreprocessgpStatus = 'red_status';
-      },
-      () => {
-        this.loadingLoader = false;
-      }
-    );
+    this.httpClient
+      .post(environment.api + '/api/secondary_preprocessgp', formData)
+      .subscribe(
+        (res) => {
+          alert('completed successfully');
+          this.loadingLoader = false;
+          this.secondaryPreprocessgpStatus = 'green_status';
+          this.pnBoundaryCheck();
+        },
+        (err) => {
+          console.error(err);
+          alert(' completed with error');
+          this.loadingLoader = false;
+          this.secondaryPreprocessgpStatus = 'red_status';
+        },
+        () => {
+          this.loadingLoader = false;
+        }
+      );
   }
   pnBoundaryCheck() {
     this.loadingLoader = true;
     const formData = new FormData();
     formData.append('username', localStorage.getItem('username'));
-    this.httpClient.post(environment.api+'/api/secondary_pnb_fed_ug', formData).subscribe(
-      (res) => {
-        alert('completed successfully');
-        this.loadingLoader = false;
-        this.pnBoundaryCheckStatus = 'green_status';
-        this.extractExisting();
-      },
-      (err) => {
-        console.error(err);
-        alert(' completed with error');
-        this.loadingLoader = false;
-        this.pnBoundaryCheckStatus = 'red_status';
-      },
-      () => {
-        this.loadingLoader = false;
-      }
-    );
+    formData.append('schema', localStorage.getItem('username'));
+    this.httpClient
+      .post(environment.api + '/api/pnboundary_check', formData)
+      .subscribe(
+        (res: any) => {
+          alert('completed successfully');
+          this.loadingLoader = false;
+          this.pnBoundaryCheckStatus = res.fedtype;
+          this.extractExisting();
+        },
+        (err) => {
+          console.error(err);
+          alert(' completed with error');
+          this.loadingLoader = false;
+          this.pnBoundaryCheckStatus = '';
+        },
+        () => {
+          this.loadingLoader = false;
+        }
+      );
   }
 
   export() {
@@ -164,56 +173,58 @@ export class ConnectionHandelingComponent implements OnInit {
     this.loadingLoader = true;
     const formData = new FormData();
     formData.append('username', localStorage.getItem('username'));
-    this.httpClient.post(environment.api+'/api/export_output', formData).subscribe(
-      (res) => {
-        this.httpClient
-          .post('/api/download_qgis_files', formData, {
-            responseType: 'arraybuffer',
-          })
-          .subscribe(
-            (res2: any) => {
-              let blob = new Blob([res2], { type: 'application/zip' });
-              let url = window.URL.createObjectURL(blob);
-              //let pwa = window.open(url);
-              var a = document.createElement('a');
-              document.body.appendChild(a);
-              url = window.URL.createObjectURL(blob);
-              a.href = url;
-              a.download = localStorage.getItem('username') + '.zip';
-              a.click();
-              window.URL.revokeObjectURL(url);
+    this.httpClient
+      .post(environment.api + '/api/export_output', formData)
+      .subscribe(
+        (res) => {
+          this.httpClient
+            .post('/api/download_qgis_files', formData, {
+              responseType: 'arraybuffer',
+            })
+            .subscribe(
+              (res2: any) => {
+                let blob = new Blob([res2], { type: 'application/zip' });
+                let url = window.URL.createObjectURL(blob);
+                //let pwa = window.open(url);
+                var a = document.createElement('a');
+                document.body.appendChild(a);
+                url = window.URL.createObjectURL(blob);
+                a.href = url;
+                a.download = localStorage.getItem('username') + '.zip';
+                a.click();
+                window.URL.revokeObjectURL(url);
 
-              /*if (!pwa || pwa.closed || typeof pwa.closed == 'undefined') {
+                /*if (!pwa || pwa.closed || typeof pwa.closed == 'undefined') {
                   alert('Please disable your Pop-up blocker and try again.');
                 }*/
 
-              alert('completed successfully');
-              this.loadingLoader = false;
-              this.exportStatus = 'green_status';
-              document.body.removeChild(a);
-              this.UploadRefPoint();
-            },
-            (err2) => {
-              console.error(err2);
-              alert(' completed with error');
-              this.loadingLoader = false;
-              this.exportStatus = 'red_status';
-              // debugger;
-              // this.UploadRefPoint();
-            },
-            () => {
-              this.loadingLoader = false;
-            }
-          );
-      },
-      (err) => {
-        console.error(err);
-        alert(' completed with error');
-        this.loadingLoader = false;
-        this.exportStatus = 'red_status';
-      },
-      () => {}
-    );
+                alert('completed successfully');
+                this.loadingLoader = false;
+                this.exportStatus = 'green_status';
+                document.body.removeChild(a);
+                this.UploadRefPoint();
+              },
+              (err2) => {
+                console.error(err2);
+                alert(' completed with error');
+                this.loadingLoader = false;
+                this.exportStatus = 'red_status';
+                // debugger;
+                // this.UploadRefPoint();
+              },
+              () => {
+                this.loadingLoader = false;
+              }
+            );
+        },
+        (err) => {
+          console.error(err);
+          alert(' completed with error');
+          this.loadingLoader = false;
+          this.exportStatus = 'red_status';
+        },
+        () => {}
+      );
   }
 
   UploadRef() {
@@ -227,21 +238,23 @@ export class ConnectionHandelingComponent implements OnInit {
       }
     }
 
-    this.httpClient.post(environment.api+'/api/ref_point_page', formData).subscribe(
-      (res) => {
-        alert('upload completed successfully');
-        this.uploadRefFilesStatus = 'green_status';
-        this.UploadGoogle();
-      },
-      (err) => {
-        console.error(err);
-        alert(' upload completed with error');
-        this.uploadRefFilesStatus = 'red_status';
-      },
-      () => {
-        this.loadingLoader = false;
-      }
-    );
+    this.httpClient
+      .post(environment.api + '/api/ref_point_page', formData)
+      .subscribe(
+        (res) => {
+          alert('upload completed successfully');
+          this.uploadRefFilesStatus = 'green_status';
+          this.UploadGoogle();
+        },
+        (err) => {
+          console.error(err);
+          alert(' upload completed with error');
+          this.uploadRefFilesStatus = 'red_status';
+        },
+        () => {
+          this.loadingLoader = false;
+        }
+      );
   }
 
   UploadGoogle() {
@@ -256,20 +269,22 @@ export class ConnectionHandelingComponent implements OnInit {
       }
     }
     */
-    this.httpClient.post(environment.api+'/api/googleapifetch2', formData).subscribe(
-      (res) => {
-        alert('upload completed successfully');
-        this.googleFetchStatus = 'green_status';
-        this.secondaryPreprocessgp();
-      },
-      (err) => {
-        console.error(err);
-        alert(' upload completed with error');
-        this.googleFetchStatus = 'red_status';
-      },
-      () => {
-        this.loadingLoader = false;
-      }
-    );
+    this.httpClient
+      .post(environment.api + '/api/googleapifetch2', formData)
+      .subscribe(
+        (res) => {
+          alert('upload completed successfully');
+          this.googleFetchStatus = 'green_status';
+          this.secondaryPreprocessgp();
+        },
+        (err) => {
+          console.error(err);
+          alert(' upload completed with error');
+          this.googleFetchStatus = 'red_status';
+        },
+        () => {
+          this.loadingLoader = false;
+        }
+      );
   }
 }
